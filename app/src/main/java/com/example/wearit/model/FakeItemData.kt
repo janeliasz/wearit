ad2d56de3db964e44d502adfeb786d87ab7b3f40
janeliasz <eljasior@gmail.com>
2022-11-12 11:21:08 +0100

    Add Item properties and fake item data

---
+++ app/src/main/java/com/example/wearit/model/FakeItemData.kt
@@ -0,0 +1,30 @@
+package com.example.wearit.model
+
+import com.example.wearit.R
+
+val fakeItems = listOf<Item>(
+        Item(
+                id = "1",
+                name = "hat",
+                photoId = R.drawable.hat,
+                category = Category.Headgear
+        ),
+        Item(
+                id = "2",
+                name = "sweater",
+                photoId = R.drawable.sweater,
+                category = Category.Headgear
+        ),
+        Item(
+                id = "3",
+                name = "jeans",
+                photoId = R.drawable.jeans,
+                category = Category.Headgear
+        ),
+        Item(
+                id = "4",
+                name = "shoes",
+                photoId = R.drawable.shoes,
+                category = Category.Headgear
+        )
+)

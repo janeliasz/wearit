7a054f068d8341bd45715865c46bd1e8b110934f
march3 <260455@student.pwr.edu.pl>
2022-12-03 11:54:26 +0100

    updated colors and theme

--- app/src/main/java/com/example/wearit/ui/theme/Color.kt
+++ app/src/main/java/com/example/wearit/ui/theme/Color.kt
@@ -2,7 +2,10 @@ package com.example.wearit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFFDFDFDF)
+val MainRed = Color(0xFFFF4848)
+val RedVaraint = Color(0x47FF4848)
+
+val MainBlack = Color.Black
+val BlackVariant = Color(0xFF767068)
+
+val SecondaryWhite = Color.White

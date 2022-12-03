1a7179eed38404e2ff77ce9123f007f5c278d45f
march3 <260455@student.pwr.edu.pl>
2022-12-03 18:52:12 +0100

    changed category enum - error

--- app/src/main/java/com/example/wearit/model/Category.kt
+++ app/src/main/java/com/example/wearit/model/Category.kt
@@ -1,11 +1,11 @@
-package com.example.wearit.model
+import com.example.wearit.R
 
-enum class Category {
-    Headgear,
-    Coat,
-    Blouse,
-    Tshirt,
-    Trousers,
-    Shorts,
-    Boots
+enum class Category(val icon: Int) {
+    Headgear(R.drawable.hat),
+    Coat(R.drawable.coat),
+    Blouse(R.drawable.hoodie),
+    Tshirt(R.drawable.tshirt),
+    Trousers(R.drawable.trouser),
+    Shorts(R.drawable.shorts),
+    Boots(R.drawable.boots),
 }

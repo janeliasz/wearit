3f92a3e9af8ad944a3ad3f1b192a735998e6ae77
Kamil-Krawiec <kamil@Kamils-MacBook-Pro.local>
2022-12-20 15:37:40 +0100

    Picker screen ready

--- app/src/main/java/com/example/wearit/ui/PickerScreen.kt
+++ app/src/main/java/com/example/wearit/ui/PickerScreen.kt
@@ -1,20 +1,34 @@
 package com.example.wearit.ui
 
 import android.graphics.Bitmap
+import android.os.Build.VERSION.SDK_INT
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.request.ImageRequest
+import coil.size.Scale
 import com.example.wearit.R
 import com.example.wearit.components.ButtonType
 import com.example.wearit.components.MasterButton
@@ -63,7 +77,8 @@ fun PickerContent(
 ) {
     Box(
         modifier = Modifier
-            .fillMaxWidth().height(75.dp)
+            .fillMaxWidth()
+            .height(75.dp)
             .padding(10.dp),
         contentAlignment = Alignment.Center,
     ) {
@@ -84,35 +99,91 @@ fun PickerContent(
             contentScale = ContentScale.Fit
         )
     }
-
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(vertical = 100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        items(currentSelection) { item ->
+
+            Row(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                GifImage(
+                    gif = R.drawable.left,
+                    modifier = Modifier
+                        .clickable { changeSelectedItem(item.category, false) }
+                        .align(Alignment.CenterVertically)
+                        .fillParentMaxSize(0.15f)
+                )
 
 
-        LazyColumn {
-            items(currentSelection) { item ->
-                Row {
-                    Button(onClick = { changeSelectedItem(item.category, false) }) {
-                        Text(text = "PREV")
-                    }
-
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .clip(shape = RoundedCornerShape(50.dp))
+                        .align(Alignment.CenterVertically)
+                        .border(
+                            width = 5.dp,
+                            color = MaterialTheme.colors.primary.copy(alpha = LocalContentAlpha.current),
+                            shape = RoundedCornerShape(50.dp)
+                        )
+                        .fillMaxWidth(0.65f)
+                ) {
                     Image(
                         bitmap = getItemPhotoByPhotoFilename(item.photoFilename).asImageBitmap(),
                         contentDescription = item.name,
-                        modifier = Modifier.size(100.dp)
+                        modifier = Modifier.padding(20.dp),
+                        contentScale = ContentScale.Fit
                     )
-
-                    Button(onClick = { changeSelectedItem(item.category, true) }) {
-                        Text(text = "NEXT")
-                    }
                 }
+
+                GifImage(
+                    gif = R.drawable.right,
+                    modifier = Modifier
+                        .clickable { changeSelectedItem(item.category, true) }
+                        .align(Alignment.CenterVertically)
+                        .fillParentMaxSize(0.15f)
+                )
+
+
             }
         }
-
-
     }
+
+}
+
+@Composable
+fun GifImage(
+    modifier: Modifier = Modifier,
+    gif: Int,
+) {
+    val context = LocalContext.current
+    val imageLoader = ImageLoader.Builder(context)
+        .components {
+            if (SDK_INT >= 100) {
+                add(ImageDecoderDecoder.Factory())
+            } else {
+                add(GifDecoder.Factory())
+            }
+        }
+        .build()
+    Image(
+        painter = rememberAsyncImagePainter(
+            ImageRequest.Builder(context).data(data = gif).apply(block = {
+                scale(Scale.FIT)
+            }).build(), imageLoader = imageLoader
+        ),
+        contentDescription = null,
+        modifier = modifier.fillMaxWidth(),
+        contentScale = ContentScale.Crop
+    )
 }
 
 @Composable

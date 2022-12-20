c276a51e6da89a4ed71446de93490a6533961db9
Kamil-Krawiec <kamil@Kamils-MacBook-Pro.local>
2022-12-20 07:18:58 +0100

    Fixed top

--- app/src/main/java/com/example/wearit/ui/PickerScreen.kt
+++ app/src/main/java/com/example/wearit/ui/PickerScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -60,23 +61,34 @@ fun PickerContent(
     goToSettings: () -> Unit
 
 ) {
-    Column(){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth().height(75.dp)
+            .padding(10.dp),
+        contentAlignment = Alignment.Center,
+    ) {
         Image(
             painter = painterResource(id = R.drawable.wearit),
             contentDescription = "Logo text",
-            modifier = Modifier.align(Alignment.CenterHorizontally),
+            modifier = Modifier.align(Alignment.Center),
+            contentScale = ContentScale.Fit
+
         )
 
-        Image(painter = painterResource(id = R.drawable.settings),
+        Image(
+            painter = painterResource(id = R.drawable.settings),
             contentDescription = "Settings",
-            modifier = Modifier.clickable { goToSettings() }.align(Alignment.End),
+            modifier = Modifier
+                .clickable { goToSettings() }
+                .align(Alignment.CenterEnd),
+            contentScale = ContentScale.Fit
         )
     }
 
-    Column(modifier = Modifier
-        .fillMaxWidth()
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
     ) {
-
 
 
         LazyColumn {
